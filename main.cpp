c9a7d68cab608ee24ef792e5f1930cf8e60cb909
AZ <wxguidesigner@gmail.com>
2012-11-22 19:18:07 +0100

    Moved files in related src/include dirs, using wxWidgets coding convention and replacing codelite project in the next commit with premake build scripts.

--- main.cpp
+++ main.cpp
@@ -4,31 +4,34 @@
 class wxMiniApp : public wxApp
 {
 public:
-	// function called at the application initialization
-	virtual bool OnInit();
+    // function called at the application initialization
+    virtual bool OnInit();
 
-	// event handler for button click
-	void OnClick(wxCommandEvent& event) {
-		GetTopWindow()->Close();
-	}
+    // event handler for button click
+    void OnClick(wxCommandEvent& event)
+    {
+        GetTopWindow()->Close();
+    }
 };
 
-IMPLEMENT_APP(wxMiniApp);
+IMPLEMENT_APP(wxMiniApp)
 
 bool wxMiniApp::OnInit()
 {
-	// create a new frame and set it as the top most application window
-	SetTopWindow( new wxFrame( NULL, -1, wxT(""), wxDefaultPosition, wxSize( 100, 50) ) );
+    // create a new frame and set it as the top most application window
+    SetTopWindow(new wxFrame( NULL, -1, wxT(""),
+                 wxDefaultPosition, wxSize( 100, 50)));
 
-	// create new button and assign it to the main frame
-	new wxButton( GetTopWindow(), wxID_EXIT, wxT("Click!") );
+    // create new button and assign it to the main frame
+    new wxButton( GetTopWindow(), wxID_EXIT, wxT("Click!") );
 
-	// connect button click event with event handler
-	Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(wxMiniApp::OnClick) );
+    // connect button click event with event handler
+    Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED,
+            wxCommandEventHandler(wxMiniApp::OnClick));
 
-	// show main frame
-	GetTopWindow()->Show();
+    // show main frame
+    GetTopWindow()->Show();
 
-	// enter the application's main loop
-	return true;
+    // enter the application's main loop
+    return true;
 }
---
+++ src/toolpalette.cpp
@@ -0,0 +1,24 @@
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#if wxUSE_BANNERWINDOW
+
+#include "wx/toolpalette.h"
+
+#ifndef WX_PRECOMP
+    
+#endif
+
+const char wxToolPaletteNameStr[] = "wxToolPalette";
+
+wxToolPalette::wxToolPalette()
+{
+}
+
+wxToolPalette::~wxToolPalette()
+{
+}
+
